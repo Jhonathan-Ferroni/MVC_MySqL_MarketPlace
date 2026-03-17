47e16d7a141c09b49852bbd746366a3c1841ee5a
Jhonathan-Ferroni <ferroni.jhonathan2403@gmail.com>
2026-03-16 21:17:40 -0300

    Basic models created

---
+++ Marketplace/Models/Departamentos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marketplace.Models
+{
+    public class Departamentos
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
