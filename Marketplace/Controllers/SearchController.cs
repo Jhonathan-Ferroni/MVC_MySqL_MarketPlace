744eb945085a453f8f1222d878e57577cbabb629
Jhonathan-Ferroni <ferroni.jhonathan2403@gmail.com>
2026-03-17 22:14:23 -0300

    CRUD for products - Routes logic - layout with routes implemented

---
+++ Marketplace/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Marketplace/Models/Departamentos.cs
+++ Marketplace/Models/Departamentos.cs
@@ -2,7 +2,7 @@
 
 namespace Marketplace.Models
 {
-    public class Departamentos
+    public class Departments
     {
         [Key]
         public int Id { get; set; }
